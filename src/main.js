f5232fae7992330b5f25c0731cb28b8d4ba81988
namephil <15054196228@163.com>
2022-06-08 21:36:10 +0800

    完成商品分类模块

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
@@ -15,6 +16,8 @@ axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Welcom from '../components/welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Right.vue'
 import Roles from '../components/power/Roles.vue'
+import Cate from '../components/goods/Cate.vue'
 
 Vue.use(VueRouter)
 
@@ -44,6 +45,11 @@ const routes = [
         path: '/roles',
         name: 'roles',
         component: Roles
+      },
+      {
+        path: '/categories',
+        name: 'categories',
+        component: Cate
       }
     ]
   }
